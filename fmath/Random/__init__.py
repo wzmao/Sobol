a72f1fc7c37020c1cac8f5ab00c4f4d1559a0e52
Wenzhi Mao <maowenzhi@yandex.com>
2015-12-18 16:46:43 +0800

    Add base files.

---
+++ fmath/Random/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""This module contains features about Random features.
+"""
+
+__author__ = 'Fei Xie'
+
+__all__ = []
+
+
+from . import Sobol
+from .Sobol import *
+__all__.extend(Sobol.__all__)
